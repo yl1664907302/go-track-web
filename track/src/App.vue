bc5b3df8888837aea2199200149f2cc1cb08d281
chenyunhe <chenyunpeng@gc.com>
2024-08-29 17:26:28 +0800

    自建vue3手脚架

---
+++ track/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import layout from './views/layout/index.vue'
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div id="app">
+    <layout>
+      <RouterView />
+    </layout>
+  </div>
+</template>
+
---
+++ track/src/router/index.ts
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from 'vue-router'
+// import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/setup',
+      name: 'setup',
+      component: () => import('../views/setup/index.vue')
+    }
+  ]
+})
+
+export default router
