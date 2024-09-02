ec4ad76fb95853f484138861bf8dce6e2eba9e80
chenyunhe <chenyunpeng@gc.com>
2024-09-02 17:38:16 +0800

    ele

---
+++ src/views/start/index.vue
@@ -0,0 +1,17 @@
+/* eslint-disable */
+<script setup lang="ts">
+import { Form, FormSchema } from '@/components/Form'
+import { reactive } from 'vue'
+
+const schema = reactive<FormSchema[]>([
+  {
+    field: 'field1',
+    label: 'input',
+    component: 'Input'
+  }
+])
+</script>
+
+<template>
+  <Form :schema="schema" />
+</template>
