78247eea8fa48871df5079e4e53400dc27a58b35
yeling <yl3080448784@163.com>
2024-09-12 23:58:35 +0800

    日历

--- src/views/main/detail.vue
+++ src/views/main/detail.vue
@@ -1,11 +1,27 @@
 <template>
-  <h3>{{ route.query.niname }}</h3>
-  <h3>{{ route.query.receiver }}</h3>
-  <h3>{{ route.query.state }}</h3>
+  <el-row>
+    <el-col>
+      <el-space>
+        <div :style="{ width: '50%', border: '2px solid #d9d9d9', borderRadius: '15px' }">
+    <a-calendar v-model:value="value" :fullscreen="false" @panelChange="onPanelChange" />
+  </div>
+</el-space>
+  </el-col>
+  </el-row>
+
 </template>
 
 <script setup lang="ts">
 import { useRoute } from 'vue-router'
+import { ref } from 'vue'
+import { Dayjs } from 'dayjs';
 
 const route = useRoute()
+const niname = route.query.niname
+const receiver = route.query.receiver
+const state = route.query.state
+const value = ref<Dayjs>();
+const onPanelChange = (value: Dayjs, mode: string) => {
+  console.log(value, mode);
+};
 </script>
