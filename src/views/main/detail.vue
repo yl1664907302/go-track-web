48d2673200021adc203a4632ca4d4ae97b48be88
chenyunhe <chenyunpeng@gc.com>
2024-09-14 17:18:20 +0800

    完成新增详情页面机器人

--- src/views/main/detail.vue
+++ src/views/main/detail.vue
@@ -1,53 +1,107 @@
 <template>
-  <el-row>
-    <el-col>
-      <el-space>
-        <el-card class="box-card1">
-          <template #header>
-            <div class="card-header">
-              <span>消息模块：{{ niname }}<br />当前分类：{{ state }}</span>
-              <el-button
-                @click="changeAlertClass()"
-                class="button"
-                type="primary"
-                style="margin-top: -55px; margin-left: 450px"
-                >切换消息分类</el-button
-              >
-            </div>
-          </template>
-          <div class="block">
-            <span class="demonstration">选择消息查询的时间范围</span>
-            <el-date-picker
-              v-model="value2"
-              type="datetimerange"
-              start-placeholder="Start Date"
-              end-placeholder="End Date"
-              :default-time="defaultTime2"
-            />
-          </div>
-          <el-scrollbar height="600px">
-            <div class="demo-collapse">
-              <el-collapse v-model="activeNames">
-                <el-collapse-item
-                  v-for="(item, index) in collapseItems"
-                  :key="index"
-                  :title="'触发时间：' + item.title"
-                  :name="item.num"
-                >
-                  <div v-html="renderMarkdown(item.content)"> </div>
-                </el-collapse-item>
-              </el-collapse>
-            </div>
-          </el-scrollbar>
-        </el-card>
-      </el-space>
-    </el-col>
+  <el-row style="display: flex; justify-content: space-between; flex-wrap: wrap">
+    <el-card class="box-card1">
+      <template #header>
+        <div class="card-header">
+          <span>消息模块：{{ niname }}<br />当前分类：{{ state }}</span>
+          <el-button
+            @click="changeAlertClass()"
+            class="button"
+            type="primary"
+            style="margin-top: -55px; margin-left: 300px"
+            >切换消息分类</el-button
+          >
+        </div>
+      </template>
+      <div class="block">
+        <span class="demonstration">选择消息查询的时间范围</span>
+        <el-date-picker
+          v-model="value2"
+          type="datetimerange"
+          start-placeholder="Start Date"
+          end-placeholder="End Date"
+          :default-time="defaultTime2"
+        />
+      </div>
+      <el-scrollbar height="600px">
+        <div class="demo-collapse">
+          <el-collapse v-model="activeNames">
+            <el-collapse-item
+              v-for="(item, index) in collapseItems"
+              :key="index"
+              :title="'触发时间：' + item.title"
+              :name="item.num"
+            >
+              <div v-html="renderMarkdown(item.content)"> </div>
+            </el-collapse-item>
+          </el-collapse>
+        </div>
+      </el-scrollbar>
+    </el-card>
+    <el-card class="box-card2">
+      <template #header>
+        <div class="card-header">
+          <span>robot列表/创建</span>
+        </div>
+      </template>
+      <el-form :model="form" label-width="auto" style="max-width: 300px">
+        <el-form-item label="昵称">
+          <el-input v-model="form.robot_name" />
+        </el-form-item>
+        <el-form-item label="类型">
+          <el-select v-model="form.robot_class" placeholder="请选择你的webhook类型">
+            <el-option label="钉钉" value="dingtalk" />
+          </el-select>
+        </el-form-item>
+        <el-form-item label="启用">
+          <el-switch v-model="form.switch" />
+        </el-form-item>
+        <el-form-item label="token">
+          <el-input v-model="form.accesstoken" type="textarea" />
+        </el-form-item>
+        <el-form-item label="密钥">
+          <el-input v-model="form.secret" type="textarea" />
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit" style="margin-left: 128px">创建</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+    <el-card class="box-card3">
+      <template #header>
+        <div class="card-header">
+          <span>robot列表/创建</span>
+        </div>
+      </template>
+      <el-form :model="form" label-width="auto" style="max-width: 300px">
+        <el-form-item label="昵称">
+          <el-input v-model="form.robot_name" />
+        </el-form-item>
+        <el-form-item label="类型">
+          <el-select v-model="form.robot_class" placeholder="please select your zone">
+            <el-option label="钉钉" value="dingtalk" />
+          </el-select>
+        </el-form-item>
+        <el-form-item label="启用">
+          <el-switch v-model="form.switch" />
+        </el-form-item>
+        <el-form-item label="token">
+          <el-input v-model="form.accesstoken" type="textarea" />
+        </el-form-item>
+        <el-form-item label="密钥">
+          <el-input v-model="form.secret" type="textarea" />
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit" style="margin-left: 128px">创建</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
   </el-row>
 </template>
 
 <script setup lang="ts">
 import { useRoute } from 'vue-router'
-import { ref, watch, onMounted } from 'vue'
+import { ref, watch, onMounted, reactive } from 'vue'
 import { getmarkdownbystatus2time } from '@/api/login'
 import { SelectMarkDownByStatus2Time } from '@/api/login/types'
 import { marked } from 'marked'
@@ -153,6 +207,19 @@ onMounted(() => {
   const [start, end] = value2.value
   getmarkdown(receiver as string, state as string, start.toISOString(), end.toISOString())
 })
+
+// 表单处理
+const form = reactive({
+  robot_name: '',
+  robot_class: '',
+  switch: false,
+  accesstoken: '',
+  secret: ''
+})
+
+const onSubmit = () => {
+  console.log('submit!')
+}
 </script>
 
 <style scoped>
@@ -172,8 +239,19 @@ onMounted(() => {
   margin-bottom: 15px;
 }
 .box-card1 {
-  width: 600px;
+  width: 450px;
   height: 800px;
+}
+.box-card2 {
+  width: 350px;
+  height: 400px;
+  margin-left: 1250px;
+  margin-top: -1px;
+}
+.box-card3 {
+  width: 350px;
+  height: 400px;
+  margin-bottom: 20px; /* 增加底部间距以避免重叠 */
 }
 .scrollbar-demo-item {
   display: flex;
